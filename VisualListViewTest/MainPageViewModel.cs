2ac474fcae7f928a8be31c0d69f5525c26bf3e39
Nico Zhu (Shang Hai Wei Chuang Ruan Jian) <v-zhumin@microsoft.com>
2017-09-22 17:51:17 +0800

    Add project files.

---
+++ VisualListViewTest/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace VisualListViewTest
+{
+    public class MainPageViewModel
+    {
+        private ObservableCollection<Pic> _items;
+
+        public ObservableCollection<Pic> Items { get => _items; set => _items = value; }
+
+
+        public MainPageViewModel()
+        {
+            SetData();
+        }
+        private async Task SetData()
+        {
+            _items = new ObservableCollection<Pic>();
+            string root = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+            string path = root + @"\Assets\timg.jpg";
+            for (var i = 0; i < 1000; i++)
+            {
+                var tem = await getImageSource(path);
+                Items.Add(new Pic { Name = "ss", source = tem});
+            }
+        }
+        private async Task<ImageSource> getImageSource(string Path)
+        {
+            var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(Path);
+            var stream = await file.OpenReadAsync();
+            var bitmapImage = new BitmapImage();
+            await bitmapImage.SetSourceAsync(stream);
+            return bitmapImage;
+        }
+
+    }
+    public class Pic
+    {
+        public string Name { get; set; }
+        public ImageSource source { get; set; }
+    }
+}
